811d6b8da77e580aaaf3ddec365a6d06ed29fa03
oreally <o.wartlick@gmail.com>
2024-05-31 08:56:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ with st.sidebar:
         st.success('Login erfolgreich!', icon='✅')
         hf_email = st.secrets['EMAIL']
         hf_pass = st.secrets['PASS']
+        hf_token = st.secrets['HF_TOKEN']
     else:
         hf_email = st.text_input('E-mail:', type='password')
         hf_pass = st.text_input('Passwort:', type='password')
@@ -15,6 +16,7 @@ with st.sidebar:
             st.warning('Bitte einloggen!', icon='⚠️')
         else:
             st.success('Jetzt chatten!', icon='👉')
+            
     st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-an-llm-powered-chatbot-with-streamlit/)!')
 
 # Store LLM generated responses
@@ -25,3 +27,28 @@ if "messages" not in st.session_state.keys():
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
+
+# Function for generating LLM response
+def generate_response(prompt_input, hf_token):
+    headers = {"Authorization": f"Bearer {hf_token}"}
+    API_URL = "https://lucom7fjkrjfxwyk.us-east-1.aws.endpoints.huggingface.cloud" 
+    response = requests.post(API_URL, headers=headers, json=prompt_input)
+    return response.json()[0]['generated_text'].replace(prompt_input, '')
+
+# User-provided prompt
+if prompt := st.chat_input(disabled=not (hf_email and hf_pass)):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            prompt_input = {"inputs": f"Du bist ein hilfreicher Assistent. USER: {prompt} ASSISTANT:",
+              "param_grid": {"max_tokens": 500, "temperature": 0.6},
+              "options": {"wait_for_model": True}}
+            response = generate_response(prompt_input, hf_token) 
+            st.write(response) 
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
